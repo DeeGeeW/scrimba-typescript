a801f14bf2ea52989753520d2a4013fdf75804ee
DeeGeeWest <DeeGeeWest@gmail.com>
2022-08-13 18:41:11 -0400

    how to establish boolean type

--- index.ts
+++ index.ts
@@ -22,7 +22,48 @@
 //     addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)))
 
 
+// String Types mini-challenge
+// Write a function that will display the most recent reviewers name next to the review total,
+// making sure to assign a type to the parameter, to prevent unwanted behaviour.
+// : string
+
+// const reviewTotalDisplay = document.querySelector('#reviews')
+
+// const reviews = [
+//     {
+//         name: 'Sheia',
+//         stars: 5,
+//         loyaltyUser: true,
+//         date: '01-04-2021'
+//     },
+//     {
+//         name: 'Andrzej',
+//         stars: 3,
+//         loyaltyUser: false,
+//         date: '28-03-2021'
+//     },
+//     {
+//         name: 'Omar',
+//         stars: 4,
+//         loyaltyUser: true,
+//         date: '27-03-2021'
+//     },
+// ]
+
+// // Solution
+// function showReviewTotal (value : number, reviewer: string) {
+//     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
+// }
+
+// showReviewTotal(reviews.length, reviews[0].name)
+
+// Boolean Types mini-challenge
+// if the last reviewer is a loyalty User, can you add a star to the end of their name?
+// please do so in the existing function, and make sure to declare what type of 
+// parameters the function takes.
+
 const reviewTotalDisplay = document.querySelector('#reviews')
+
 
 const reviews = [
     {
@@ -46,8 +87,10 @@ const reviews = [
 ]
 
 // Solution
-function showReviewTotal (value : number, reviewer: string) {
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
+function showReviewTotal (value : number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '*' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + iconDisplay
 }
 
-showReviewTotal(reviews.length, reviews[0].name)
+
+showReviewTotal(reviews.length, reviews[0].name,  reviews[0].loyaltyUser)
