da7be25fd6bacacd88544d9e21edb9e746f81152
DeeGeeWest <DeeGeeWest@gmail.com>
2022-08-13 18:59:52 -0400

    having trouble seeing things render

--- index.ts
+++ index.ts
@@ -62,8 +62,49 @@
 // please do so in the existing function, and make sure to declare what type of 
 // parameters the function takes.
 
-const reviewTotalDisplay = document.querySelector('#reviews')
+// const reviewTotalDisplay = document.querySelector('#reviews')
 
+
+// const reviews = [
+//     {
+//         name: 'Sheia',
+//         stars: 5,
+//         loyaltyUser: true,
+//         date: '01-04-2021'
+//     },
+//     {
+//         name: 'Andrzej',
+//         stars: 3,
+//         loyaltyUser: false,
+//         date: '28-03-2021'
+//     },
+//     {
+//         name: 'Omar',
+//         stars: 4,
+//         loyaltyUser: true,
+//         date: '27-03-2021'
+//     },
+// ]
+
+// // Solution
+// function showReviewTotal (value : number, reviewer: string, isLoyalty: boolean) {
+//     const iconDisplay = isLoyalty ? '*' : ''
+//     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + iconDisplay
+// }
+
+
+// showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+// Fixing the Website
+
+// Can you fix my code to show 'Welcome back Bobby' in the Nav Bar for 
+// our couch surfing website? Do this by assigning types to the parameters
+// we pass to our populateUser function, so that we can be aware of
+// errors in our user objects in the future.
+
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
+const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
     {
@@ -86,11 +127,30 @@ const reviews = [
     },
 ]
 
-// Solution
-function showReviewTotal (value : number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? '*' : ''
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + iconDisplay
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay!.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
 }
 
 
-showReviewTotal(reviews.length, reviews[0].name,  reviews[0].loyaltyUser)
+function populateUser(isReturning : boolean, userName : string ) {
+    if (isReturning){
+        returningUserDisplay!.innerHTML = 'back'
+    }
+    userNameDisplay!.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
+
+
