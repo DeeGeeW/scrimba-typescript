ec7c9c2bc3ac605c007dc3f8df489c9e0fe9c226
DeeGeeWest <DeeGeeWest@gmail.com>
2022-08-13 14:17:09 -0400

    assigning variables in typescript small refresher on getting types in JS

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // types in JavaScript
 
-function response(a, b) {
-  return a + b;
-}
+// function response(a, b) {
+//   return a + b;
+// }
 // only put in numbers!
-console.log(response(5, 6));
+// console.log(response(5, 6));
+
+// Using typeof
+
+const button = document.querySelector(".button");
+const firstInput = document.querySelector("#first-input");
+const secondInput = document.querySelector("#second-input");
+const screen = document.querySelector(".screen");
+
+// function addNumbers(a, b) {
+//   if (typeof a === "number" && typeof b === "number") {
+//     screen.innerHTML = a + b;
+//   } else {
+//     screen.innerHTML = parseInt(a) + parseInt(b);
+//   }
+// }
+function addNumbers(a, b) {
+  screen.innerHTML = a + b;
+}
+
+button.addEventListener("click", () => addNumbers(firstInput.value, secondInput.value));
